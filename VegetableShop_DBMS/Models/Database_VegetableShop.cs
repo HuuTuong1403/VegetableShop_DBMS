463a6cce4ea1bfad00c3b1526584651665aa3157
Nguyễn Hữu Tường <64581048+tuong1403@users.noreply.github.com>
2021-01-16 22:51:49 +0700

    Tuong: Add datasource của tao nè

--- VegetableShop_DBMS/Models/Database_VegetableShop.cs
+++ VegetableShop_DBMS/Models/Database_VegetableShop.cs
@@ -10,9 +10,9 @@ namespace VegetableShop_DBMS.Models
 {
     class Database_VegetableShop
     {
-        string strConnectionString = "Data Source=DESKTOP-30KMJ3I;Initial Catalog=Database_VegetableStore;Integrated Security=True";
-        //string strConnectionString = "Data Source=DESKTOP-30KMJ3I;Initial Catalog=Database_VegetableStore;IntegratedSecurity=True";
-        
+        //string strConnectionString = "Data Source=DESKTOP-30KMJ3I;Initial Catalog=Database_VegetableStore;Integrated Security=True";
+        string strConnectionString = "Data Source=DESKTOP-ID9RBP3;Initial Catalog=Database_VegetableStore;IntegratedSecurity=True";
+
         //Tạo kết nối
         SqlConnection conn = null;
         //Bộ chuyển đổi
