be3e1ba34d80f969aacf75fb7cceab53ed419095
HoaiThu0801 <dangnguyenhoaithu08012000@gmail.com>
2021-01-18 21:45:02 +0700

    Thu: Register Seller

---
+++ VegetableShop_DBMS/Controllers/AdminSettingController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VegetableShop_DBMS.Models;
+
+
+namespace VegetableShop_DBMS.Controllers
+{
+    class AdminSettingController
+    {
+        public static bool Register_Seller(string UserName, string PassWord, string FullName, string Gender, DateTime DateofBirth,
+            string PhoneNumber, string Email, string Image, ref string err)
+        {
+            Database_VegetableShop _db = new Database_VegetableShop();
+            return _db.MyExecuteNonQuery("execute RegisterSeller N'" + UserName + "',N'" + PassWord + "',N'" + FullName + "',N'" + Gender + "',N'" + DateofBirth + "',N'" + PhoneNumber + "',N'" + Email + "',N'" + Image + "'", CommandType.Text, ref err);
+        }
+        public static bool AddItems(string ItemName, float ImportPrice, float SalePrice, string Description, ref string err)
+        {
+            Database_VegetableShop _db = new Database_VegetableShop();
+            return _db.MyExecuteNonQuery("execute AddItems N'" + ItemName + "'," + ImportPrice + " , " + SalePrice + " , N'" + Description + "'", CommandType.Text, ref err)
+        }
+    }
+}
--- VegetableShop_DBMS/Views/frmRegisterSeller.cs
+++ VegetableShop_DBMS/Views/frmRegisterSeller.cs
@@ -7,14 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using VegetableShop_DBMS.Controllers;
 namespace VegetableShop_DBMS.Views
 {
     public partial class frmRegisterSeller : Form
     {
+        string err;
         public frmRegisterSeller()
         {
             InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string UserName = txtUsername.Text.Trim();
+            string PassWord = txtPassword.Text.Trim();
+            string FullName = txtFullName.Text.Trim();
+            string Gender = cbbGender.SelectedItem.ToString();
+            DateTime DateofBirth = dtpDateOfBirth.Value;
+            string PhoneNumber = txtPhone.Text.Trim();
+            string Email = txtEmail.Text.Trim();
+            string Image = "";
+
+            bool check = AdminSettingController.Register_Seller(UserName, PassWord, FullName, Gender, DateofBirth, PhoneNumber, Email, Image, ref err);
+            if (check == true)
+            {
+                DialogResult dialogResult;
+                dialogResult = MessageBox.Show("Bạn đã đăng ký nhân viên thành công", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Đăng ký thất bại, xin thử lại lần nữa", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
         }
     }
 }
